3a9f4725e119a28bbbc53c79cd7e5d6f34f74e2f
Ismael Barragán <ismael@recursivamente.com>
2023-12-01 10:30:59 -0600

    Bootstrap repo for day 01

---
+++ days/day01/index.ts
@@ -0,0 +1,9 @@
+// https://adventofcode.com/2023/day/1
+// Day 1:
+
+import { readInput } from '../../common/index';
+
+const part01Input = readInput('days/day01/input01', '\n\n');
+
+process.stdout.write('Part 01: \n');
+process.stdout.write('Part 02: \n');
