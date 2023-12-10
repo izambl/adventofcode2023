cf16d880d4dfc50bfb546d360ac033690f1b5b8b
Ismael b. <ismael@recursivamente.com>
2023-12-10 14:40:34 -0600

    Day 10 -- part 01

---
+++ days/day10/index.ts
@@ -0,0 +1,127 @@
+// https://adventofcode.com/2023/day/10
+// Day 10: Pipe Maze
+
+import { readInput } from '../../common/index';
+
+const input = readInput('days/day10/input01', '\n').map((row) => row.split('')) as Array<TileType[]>;
+
+const pipeDescriptions: {
+  [key in TileType]: { north: boolean; south: boolean; west: boolean; east: boolean };
+} = {
+  '|': { north: true, south: true, west: false, east: false },
+  '-': { north: false, south: false, west: true, east: true },
+  L: { north: true, south: false, west: false, east: true },
+  J: { north: true, south: false, west: true, east: false },
+  '7': { north: false, south: true, west: true, east: false },
+  F: { north: false, south: true, west: false, east: true },
+  '.': { north: false, south: false, west: false, east: false },
+  S: { north: true, south: true, west: true, east: true },
+};
+
+type TileType = '|' | '-' | 'L' | 'J' | '7' | 'F' | '.' | 'S';
+type Tile = { north?: Tile; south?: Tile; west?: Tile; east?: Tile; type: TileType };
+type Map = { [key: string]: Tile };
+
+const map: Map = {};
+let startPosition: Tile = null;
+
+// Create Map
+for (const [y, row] of input.entries()) {
+  for (const [x, tileDescription] of row.entries()) {
+    const coordinate = `${String(x).padStart(3, '0')}:${String(y).padStart(3, '0')}`;
+
+    const tile: Tile = {
+      type: tileDescription,
+    };
+
+    map[coordinate] = tile;
+  }
+}
+
+// Fill Map
+for (const mapTileKey of Object.keys(map)) {
+  const mapTile = map[mapTileKey];
+  const [mapX, mapY] = mapTileKey.split(':').map(Number);
+
+  if (mapTile.type === 'S') {
+    startPosition = mapTile;
+    //continue;
+  }
+
+  if (pipeDescriptions[mapTile.type].north) {
+    const mapPosition = map[`${String(mapX).padStart(3, '0')}:${String(mapY - 1).padStart(3, '0')}`];
+    if (mapPosition && mapPosition.type !== '.') mapTile.north = mapPosition;
+  }
+  if (pipeDescriptions[mapTile.type].south) {
+    const mapPosition = map[`${String(mapX).padStart(3, '0')}:${String(mapY + 1).padStart(3, '0')}`];
+    if (mapPosition && mapPosition.type !== '.') mapTile.south = mapPosition;
+  }
+  if (pipeDescriptions[mapTile.type].west) {
+    const mapPosition = map[`${String(mapX - 1).padStart(3, '0')}:${String(mapY).padStart(3, '0')}`];
+    if (mapPosition && mapPosition.type !== '.') mapTile.west = mapPosition;
+  }
+  if (pipeDescriptions[mapTile.type].east) {
+    const mapPosition = map[`${String(mapX + 1).padStart(3, '0')}:${String(mapY).padStart(3, '0')}`];
+    if (mapPosition && mapPosition.type !== '.') mapTile.east = mapPosition;
+  }
+}
+
+// Infer Start Type
+if (startPosition.north?.south === startPosition) {
+  if (startPosition.south?.north === startPosition) {
+    startPosition.type = '|';
+    delete startPosition.west;
+    delete startPosition.east;
+  }
+  if (startPosition.west?.east === startPosition) {
+    startPosition.type = 'J';
+    delete startPosition.south;
+    delete startPosition.east;
+  }
+  if (startPosition.east?.west === startPosition) {
+    startPosition.type = 'L';
+    delete startPosition.west;
+    delete startPosition.south;
+  }
+} else if (startPosition.south?.north === startPosition) {
+  if (startPosition.west?.east === startPosition) {
+    startPosition.type = '7';
+    delete startPosition.north;
+    delete startPosition.east;
+  }
+  if (startPosition.east?.west === startPosition) {
+    startPosition.type = 'F';
+    delete startPosition.north;
+    delete startPosition.west;
+  }
+} else if (startPosition.west?.east === startPosition) {
+  if (startPosition.east?.west === startPosition) {
+    startPosition.type = '-';
+    delete startPosition.north;
+    delete startPosition.south;
+  }
+}
+
+let steps = 1;
+let runner01: Tile;
+const runnerVisits: Set<Tile> = new Set([startPosition]);
+
+[runner01] = [startPosition.north, startPosition.south, startPosition.west, startPosition.east].filter(
+  (direction) => !!direction,
+);
+
+while (runner01) {
+  runnerVisits.add(runner01);
+
+  runner01 = [runner01.north, runner01.south, runner01.west, runner01.east].filter(
+    (direction) => !!direction && !runnerVisits.has(direction),
+  )[0];
+
+  steps++;
+}
+
+const part01 = steps / 2;
+process.stdout.write(`Part 01: ${part01.toString()}\n`);
+
+const part02 = 0;
+process.stdout.write(`Part 02: ${part02.toString()}\n`);
