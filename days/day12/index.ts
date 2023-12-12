e1fd0bdac7c1e4de58e0ad101fdda8bcd8b78e4f
Ismael Barragán <ismael@recursivamente.com>
2023-12-12 17:02:52 -0600

    Day 12 - part 02 brute force?

--- days/day12/index.ts
+++ days/day12/index.ts
@@ -3,21 +3,45 @@
 
 import { readInput } from '../../common/index';
 
-const input: Array<string[]> = readInput('days/day12/input01', '\n').map((row) => row.split(' '));
+const input: Array<string[]> = readInput('days/day12/inputDemo', '\n').map((row) => row.split(' '));
 type Spring = { conditionRecord: string[]; condition: number[] };
 
 const springs: Spring[] = input.map(([springsString, conditionString]) => {
   return { conditionRecord: springsString.split(''), condition: conditionString.split(',').map(Number) };
 });
 
-function buildCondition(condition: number[]): RegExp {
-  let regexp = '^\\.*';
+function buildFinalCondition(condition: number[]): RegExp {
+  const regexp: string[] = [];
 
-  regexp += condition.map((num) => '#'.repeat(num)).join('\\.+');
+  regexp.push('^\\.*');
+  regexp.push(condition.map((num) => `#{${num}}`).join('\\.+'));
+  regexp.push('\\.*$');
 
-  regexp += '\\.*$';
+  return new RegExp(regexp.join(''));
+}
 
-  return new RegExp(regexp);
+function buildTestCondition(condition: number[]): RegExp {
+  const regexp: string[] = [];
+
+  regexp.push('^\\.*');
+  regexp.push(condition.map((num) => `[?#]{${num}}`).join('\\.+'));
+  regexp.push('\\.*$');
+
+  return new RegExp(regexp.join(''));
+}
+
+function copyConditions(conditionRecord: string[], condition: number[], times: number): [string[], number[]] {
+  const newConditionRecord = [...Array(times)]
+    .map(() => conditionRecord.join(''))
+    .join('?')
+    .split('');
+  const newCondition = [...Array(times)]
+    .map(() => condition.join(','))
+    .join(',')
+    .split(',')
+    .map(Number);
+
+  return [newConditionRecord, newCondition];
 }
 
 function walkCondition(
@@ -28,6 +52,9 @@ function walkCondition(
   startPosition = 0,
 ): number {
   let string = totalString;
+  if (record.length - startPosition + string.length !== record.length) {
+    return 1;
+  }
 
   for (let i = startPosition; i < record.length; i++) {
     const rec = record[i];
@@ -54,11 +81,21 @@ function log() {
   validArrangement += 1;
 }
 for (const { conditionRecord, condition } of springs) {
-  walkCondition(conditionRecord, buildCondition(condition), log);
+  walkCondition(conditionRecord, buildFinalCondition(condition), log);
 }
-
 const part01 = validArrangement;
 process.stdout.write(`Part 01: ${part01}\n`);
 
-const part02 = 1;
+let validArrangement2 = 0;
+function log2() {
+  validArrangement2 += 1;
+}
+for (const { conditionRecord, condition } of springs) {
+  const [newConditionRecord, newCondition] = copyConditions(conditionRecord, condition, 5);
+
+  walkCondition(newConditionRecord, buildFinalCondition(newCondition), log2);
+  console.log(validArrangement2);
+}
+
+const part02 = validArrangement2;
 process.stdout.write(`Part 02: ${part02}\n`);
