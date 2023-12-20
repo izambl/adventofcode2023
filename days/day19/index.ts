0709ce789daf5d99d3c38b283dff6a2d16540cb4
Ismael Barragán <ismael@recursivamente.com>
2023-12-19 22:25:49 -0600

    Day 19 - part01 improved

--- days/day19/index.ts
+++ days/day19/index.ts
@@ -1,25 +1,8 @@
 // https://adventofcode.com/2023/day/19
 // Day 19: Aplenty
 
+import { inspect } from 'util';
 import { readInput } from '../../common/index';
-
-const [workflowsInput, partsRatings] = readInput('days/day19/input01', '\n\n');
-
-type WorkFlow = Array<string[]>;
-type WorkFlows = { [key: string]: WorkFlow };
-const workFlows: WorkFlows = workflowsInput.split('\n').reduce((allWorkflows, workflowLine) => {
-  const [workflowKey, workflowFlow] = workflowLine.slice(0, -1).split('{');
-
-  allWorkflows[workflowKey] = workflowFlow.split(',').map((flow) => {
-    const flowValues = flow.split(/[<>:]/);
-    if (flowValues.length === 3) {
-      flowValues.push(flow.slice(1, 2));
-    }
-    return flowValues;
-  });
-
-  return allWorkflows;
-}, {} as WorkFlows);
 
 type Part = {
   x: number;
@@ -27,6 +10,28 @@ type Part = {
   a: number;
   s: number;
 };
+type Category = 'x' | 'm' | 'a' | 's';
+type Operator = '<' | '>';
+type Condition = [Category, Operator, number];
+type Rule = {
+  condition: Condition;
+  if: Rule | string;
+  else: Rule | string;
+};
+
+function normalizeRule(rule: Rule): Rule {
+  if (rule.if !== 'R' && rule.if !== 'A') {
+    rule.if = typeof rule.if === 'string' ? normalizeRule(rulesDict[rule.if]) : normalizeRule(rule.if);
+  }
+
+  if (rule.else !== 'R' && rule.else !== 'A') {
+    rule.else = typeof rule.else === 'string' ? normalizeRule(rulesDict[rule.else]) : normalizeRule(rule.else);
+  }
+
+  return rule;
+}
+
+const [workflowsInput, partsRatings] = readInput('days/day19/inputDemo', '\n\n');
 const parts: Part[] = partsRatings.split('\n').map((partString) => {
   let jsonString = partString.replace(/=/g, ':');
   jsonString = jsonString.replace(/x/g, '"x"');
@@ -37,30 +42,56 @@ const parts: Part[] = partsRatings.split('\n').map((partString) => {
   return JSON.parse(jsonString);
 });
 
-function executeFlow(part: Part, workFlow: WorkFlow): string {
-  for (const workFlowStep of workFlow) {
-    if (workFlowStep.length === 1) return workFlowStep[0];
-    const [category, quantity, nextWorkFlow, operator] = workFlowStep;
+const rulesDict: { [key: string]: Rule } = {};
 
-    if (operator === '<') {
-      if (part[category as keyof Part] < Number(quantity)) return nextWorkFlow;
+for (const workFlow of workflowsInput.split('\n')) {
+  const [workflowKey, workflowFlow] = workFlow.slice(0, -1).split('{');
+
+  const ruleParts = workflowFlow.split(',');
+
+  const elsePart = ruleParts.pop();
+  const firstPart = ruleParts.pop();
+  const [condition, ifPart] = firstPart.split(':');
+  let newRule: Rule = {
+    condition: [condition[0] as Category, condition[1] as Operator, Number(condition.slice(2))],
+    if: ifPart,
+    else: elsePart,
+  };
+
+  while (ruleParts.length) {
+    const firstPart = ruleParts.pop();
+    const [condition, ifPart] = firstPart.split(':');
+    newRule = {
+      condition: [condition[0] as Category, condition[1] as Operator, Number(condition.slice(2))],
+      if: ifPart,
+      else: newRule,
+    };
+  }
+
+  rulesDict[workflowKey] = newRule;
+}
+
+const startRule = normalizeRule(rulesDict.in);
+
+console.log(inspect(startRule, false, null, true));
+
+let part01b = 0;
+for (const part of parts) {
+  let currentResult: Rule | string = startRule;
+  const { x, m, a, s } = part;
+
+  while (typeof currentResult !== 'string') {
+    const [category, operator, value]: Condition = currentResult.condition;
+    if (operator === '>') {
+      currentResult = part[category] > value ? currentResult.if : currentResult.else;
     } else {
-      if (part[category as keyof Part] > Number(quantity)) return nextWorkFlow;
+      currentResult = part[category] < value ? currentResult.if : currentResult.else;
     }
   }
-}
 
-let part01 = 0;
-for (const part of parts) {
-  let currentFlow = 'in';
-
-  while (currentFlow !== 'R' && currentFlow !== 'A') {
-    currentFlow = executeFlow(part, workFlows[currentFlow]);
-  }
-
-  if (currentFlow === 'A') {
-    part01 += part.x + part.m + part.a + part.s;
+  if (currentResult === 'A') {
+    part01b += x + m + a + s;
   }
 }
 
-process.stdout.write(`Part 01: ${part01}\n`);
+process.stdout.write(`Part 01b: ${part01b}\n`);
