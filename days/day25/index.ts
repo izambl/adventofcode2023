543fd1b41b9de5cbebae7a0c69521f0c0d5075c4
Ismael b. <ismael@recursivamente.com>
2024-02-09 13:32:48 -0600

    Day 25 -- wip

---
+++ days/day25/index.ts
@@ -0,0 +1,12 @@
+// https://adventofcode.com/2023/day/25
+// Day 25: Snowverload
+
+import { readInput } from '../../common/index';
+
+const input = readInput('days/day25/input01', '\n');
+
+const part01 = 1;
+process.stdout.write(`Part 01: ${part01}\n`);
+
+const part02 = 2;
+process.stdout.write(`Part 02: ${part02}\n`);
